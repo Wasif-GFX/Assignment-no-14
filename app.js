4b421099e2f18b4d123e0d2bda244c01a2211fc8
Wasif <111518026+Wasif-GFX@users.noreply.github.com>
2024-09-13 01:13:02 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,71 @@
+// var city = prompt("Enter your City Name")
+// if(city === "karachi"){
+//     alert("Welcome to city of light")
+// }
+// else{
+//     alert(`Wwlcome to city ${city}`)
+// }
+
+// var gender= prompt("Your Gender")
+// if(gender === "male"){
+//     alert("Good Morning Sir")
+// }
+// else if(gender === "female"){
+//     alert("Good Morning Ma'am")
+// }
+// else if (gender = " "){
+//     alert("Erorr")
+// }
+
+// var signal = prompt("ENter trfice Signal Color")
+
+// if(signal === "red"){
+//     alert("Must Top")
+// } else if(signal === "yellow"){
+//     alert("Redy to move")
+// } else if(signal === "green"){
+//     alert("Move to Now")
+// }else if (signal = " "){
+//     alert("please check you Signal Color")
+// }
+
+var a = 4;
+if (++a === 5) {
+    alert("given condition for variable a is true");
+}
+
+var b = 82;
+if (b++ === 83) {
+    alert("given condition for variable b is true");
+}
+
+var c = 12;
+if (c++ === 13) {
+    alert("condition 1 is true");
+}
+
+if (c === 13) {
+    alert("condition 2 is true");
+}
+if (++c < 14) {
+    alert("condition 3 is true");
+}
+if (c === 14) {
+    alert("condition 4 is true");
+}
+var materialCost = 20000;
+var laborCost = 2000;
+var totalCost = materialCost + laborCost;
+if (totalCost === laborCost + materialCost) {
+    alert("The cost equals");
+}
+if (true) {
+    alert("True");
+}
+if (false) {
+    alert("False");
+}
+
+if("car" < "cat"){
+    alert("car is smaller than cat");
+}
